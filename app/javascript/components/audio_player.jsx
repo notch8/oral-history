d27b626d3cf5b4b7e1f92a4f25f3e5f54d891f72
Rob Kaufman <rob@notch8.com>
2017-12-19 12:47:16 -0800

    play buttons

---
+++ app/javascript/components/audio_player.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import Wavesurfer from 'react-wavesurfer';
+
+export default class AudioPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      audioFile: this.props.url,
+      playing: false,
+      pos: 0,
+      volume: 1,
+      audioRate: 1
+    };
+    this.handleTogglePlay = this.handleTogglePlay.bind(this);
+    this.handlePosChange = this.handlePosChange.bind(this);
+    this.handleReady = this.handleReady.bind(this);
+    this.handleVolumeChange = this.handleVolumeChange.bind(this);
+    this.handleAudioRateChange = this.handleAudioRateChange.bind(this);
+  }
+
+  handleAudioRateChange(e) {
+    this.setState({
+      audioRate: +e.target.value
+    });
+  }
+
+  handleTogglePlay() {
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+
+  handlePosChange(e) {
+    this.setState({
+      pos: e.originalArgs ? e.originalArgs[0] : +e.target.value
+    });
+  }
+
+  handleReady() {
+    this.setState({
+      pos: 0
+    });
+  }
+
+  handleVolumeChange(e) {
+    this.setState({
+      volume: +e.target.value
+    });
+  }
+
+  handleNewUrl(e) {
+    this.setState({
+      audioFile: e.detail.url
+    })
+  }
+
+  componentDidMount() {
+    window.addEventListener('newurl', this.handleNewUrl)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('newurl', this.handleNewUrl)
+  }
+
+  render() {
+    const waveOptions = {
+      scrollParent: true,
+      progressColor: '#c2daeb',
+      waveColor: '#1e4b87',
+      normalize: true,
+      audioRate: 1,
+      height: 340,
+      barWidth: 2,
+    };
+    var playPause = (this.state.playing ? 'pause-button' : 'play-button')
+    return (
+      <div className="player col-xs-12">
+        <div className="col-xs-4">
+          <a onClick={this.handleTogglePlay} className={playPause}>
+            <img src={this.props.image} className='img-responsive' />
+          </a>
+        </div>
+        <div className='col-xs-8 wave-box'>
+          <Wavesurfer
+            volume={this.state.volume}
+            pos={this.state.pos}
+            options={waveOptions}
+            onPosChange={this.handlePosChange}
+            audioFile={this.state.audioFile}
+            playing={this.state.playing}
+            onReady={this.handleReady}
+          />
+        </div>
+      </div>
+    );
+  }
+}
