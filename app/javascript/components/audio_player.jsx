5117aa3400b18b19aafb903b7358c1ebc0fd6b18
George Wheeler <george@dewey4.com>
2018-06-22 14:08:02 -0700

    working player w/ changing sources

--- app/javascript/components/audio_player.jsx
+++ app/javascript/components/audio_player.jsx
@@ -1,105 +1,143 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import Wavesurfer from 'react-wavesurfer';
+import Hls from 'hls.js'
 
-export default class AudioPlayer extends React.Component {
+const waveOptions = {
+  container: '.wave-box',
+  backend: 'MediaElement',
+  progressColor: '#c2daeb',
+  waveColor: '#1e4b87',
+  audioRate: 1,
+  height: 340,
+  barWidth: 2
+}
+
+export default class AudioPlayer extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+
+    const { url } = this.props
 
     this.state = {
-      audioFile: this.props.url,
+      source: url,
       playing: false,
-      pos: 0,
       volume: 1,
-      audioRate: 1
-    };
-    this.handleTogglePlay = this.handleTogglePlay.bind(this);
-    this.handlePosChange = this.handlePosChange.bind(this);
-    this.handleReady = this.handleReady.bind(this);
-    this.handleVolumeChange = this.handleVolumeChange.bind(this);
-    this.handleAudioRateChange = this.handleAudioRateChange.bind(this);
-  }
+    }
 
-  handleAudioRateChange(e) {
-    this.setState({
-      audioRate: +e.target.value
-    });
-  }
-
-  handleTogglePlay() {
-    this.setState({
-      playing: !this.state.playing
-    });
-  }
-
-  handlePosChange(e) {
-    this.setState({
-      pos: e.originalArgs ? e.originalArgs[0] : +e.target.value
-    });
-  }
-
-  handleReady() {
-    this.setState({
-      pos: 0
-    });
-  }
-
-  handleVolumeChange(e) {
-    this.setState({
-      volume: +e.target.value
-    });
-  }
-
-  handleNewUrl(e) {
-    this.setState({
-      audioFile: e.detail.url,
-      pos: 0,
-      playing: false
-    })
-    setTimeout(function() {
-      this.handleTogglePlay() // TODO is this timeout long enough? is there a better way??
-    }.bind(this), 3000)
-  }
-
-  componentDidMount() {
-    window.addEventListener('newurl', this.handleNewUrl.bind(this))
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('newurl', this.handleNewUrl.bind(this))
+    this.handleTogglePlay = this.handleTogglePlay.bind(this)
+    this.changeVol = this.changeVol.bind(this)
   }
 
   render() {
-    const waveOptions = {
-      scrollParent: true,
-      progressColor: '#c2daeb',
-      waveColor: '#1e4b87',
-      normalize: true,
-      audioRate: 1,
-      height: 340,
-      barWidth: 2,
-    };
-    var playPause = (this.state.playing ? 'pause-button' : 'play-button')
+    const { volume, pos, source, playing, sliderPos } = this.state
+
+    var playPause = (playing ? 'pause-button' : 'play-button')
+
     return (
       <div className="player col-xs-12">
+        <audio id="audio" ref="audio" src={source} style={{display: 'none'}}></audio>
         <div className="col-xs-4">
-          <a onClick={this.handleTogglePlay} className={playPause}>
-            <img src={this.props.image} className='img-responsive' />
-          </a>
+          <img src={this.props.image} className='img-responsive' />
+          <a onClick={this.handleTogglePlay} className={playPause}></a>
+          <div id="volume-slider" className="volume-slider" onClick={this.changeVol} onDrag={this.changeVol}>
+            <div style={{left: sliderPos || '96%'}} className="marker"></div>
+            <div style={{width: `${(volume * 100)}%` || '50%'}} className="fill"></div>
+          </div>
         </div>
         <div className='col-xs-8 wave-box'>
-          <Wavesurfer
-            volume={this.state.volume}
-            pos={this.state.pos}
-            options={waveOptions}
-            onPosChange={this.handlePosChange}
-            audioFile={this.state.audioFile}
-            playing={this.state.playing}
-            onReady={this.handleReady}
-          />
         </div>
       </div>
-    );
+    )
   }
+
+  changeVol(e) {
+    let b = document.getElementById('volume-slider').getClientRects()[0]
+    let { audio } = this.refs
+
+    const volume = computeVolume(e, b)
+
+    audio.volume = volume
+
+    this.setState({
+      volume,
+      sliderPos: e.clientX - b.left - 8, // some math to center the mark on the pointer
+    })
+  }
+
+  handleTogglePlay() {
+    let { playing } = this.state
+    let { audio } = this.refs
+
+    playing = !playing
+
+    this.setState({ playing })
+
+    if (playing) {
+      audio.play()
+    } else {
+      audio.pause()
+    }
+  }
+
+  componentDidMount() {
+    const { source } = this.state
+    let { audio } = this.refs
+
+    let hls = new Hls()
+    hls.loadSource(source)
+    hls.attachMedia(audio)
+
+    let wavesurfer = window.WaveSurfer.create(waveOptions)
+
+    loadPeaks(audio, wavesurfer)
+
+    window.addEventListener('set_audio_player_src', (e) => {
+      hls.detachMedia()
+      hls.loadSource(e.detail.url)
+      hls.attachMedia(audio)
+
+      loadPeaks(audio, wavesurfer)
+
+      this.setState({
+        playing: false,
+      })
+
+      audio.oncanplay = () => {
+        audio.play()
+
+        this.setState({
+          playing: true,
+        })
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('set_audio_player_src', this.handleNewSrc.bind(this))
+  }
+}
+
+const loadPeaks = function(element, wavesurfer) {
+  wavesurfer.util.ajax({
+      responseType: 'json',
+      url: '/peaks.json'
+  })
+  .on('success', function(data) {
+    wavesurfer.load(element, data);
+  })
+}
+
+const computeVolume = (e, b) => {
+  let volume = ((e.clientX - b.left) / b.width)
+
+  if (volume < 0) {
+    volume = 0
+  }
+
+  if (volume > 1) {
+    volume = 1
+  }
+
+  return volume
 }
--- app/javascript/components/play_button.jsx
+++ app/javascript/components/play_button.jsx
@@ -3,18 +3,8 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 export default class PlayButton extends React.Component {
-  handleOnClick(e){
-    var newUrl = this.props.url;
-    var event = new CustomEvent('newurl',
-                                { bubbles: true,
-                                  cancelable: true,
-                                  detail: { url: newUrl}
-                                });
-    window.dispatchEvent(event);
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <div className="player">
         <a className="play-icon" onClick={this.handleOnClick.bind(this)}>
           <i className="fa fa-play-circle-o g-font-size-18"></i>
@@ -23,4 +13,17 @@ export default class PlayButton extends React.Component {
     )
   }
 
+  handleOnClick(e) {
+    var newUrl = this.props.url
+    var event = new CustomEvent(
+      'set_audio_player_src',
+      {
+        bubbles: true,
+        cancelable: true,
+        detail: { url: newUrl }
+      },
+    )
+
+    window.dispatchEvent(event)
+  }
 }
