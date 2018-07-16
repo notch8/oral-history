5566b42e25443cf63981bd4fc4228eac0c9a1985
George Wheeler <george@dewey4.com>
2018-07-16 11:38:01 -0700

    fix config

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
-Delayed::Job.destroy_failed_jobs = false
+Delayed::Worker.destroy_failed_jobs = false
 
 silence_warnings do
   Delayed::Job.const_set("MAX_ATTEMPTS", 10)
-  Delayed::Job.const_set("MAX_RUN_TIME", 30.minutes)
+  Delayed::Job.const_set("MAX_RUN_TIME", 4.hours)
 end
