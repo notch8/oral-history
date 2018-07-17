25e9a3d215bb640fa62a5a6dfbf34910f9a69a67
Lea Ann Bradford <leaann@notch8.com>
2018-07-17 11:35:36 -0700

    added error handling and messaging and loading to transcript displayer

--- app/javascript/components/file_view.jsx
+++ app/javascript/components/file_view.jsx
@@ -9,15 +9,32 @@ export default class FileView extends Component {
     super(props)
     this.state = {
       timelog: '',
+      transcriptShow: false,
+      isLoading: false,
+      error: null,
     }
   }
 
   render() {
     return(
-      <div dangerouslySetInnerHTML={{ __html: this.state.timelog}} /> 
+      <div>
+        { this.state.error &&
+          <div className="container transcript-container-loading text-danger g-brd-around g-brd-red rounded-0">{ this.state.error.message }</div>
+        }
+        { this.state.isLoading && 
+          <div className="container g-brd-primary g-brd-around rounded-0 transcript-container-loading">Transcripts are loading...</div>
+        }
+        { this.state.transcriptShow ? (
+          <div className="container g-brd-primary g-brd-around rounded-0 transcript-container" dangerouslySetInnerHTML={{ __html: this.state.timelog}} /> 
+          ) : (
+          <div></div>
+          ) 
+        }
+      </div>
     )
   }
-    
+  
+
   componentDidMount() {
     let handler = this.changeFile.bind(this)
     window.addEventListener('set_audio_player_src', handler)
@@ -30,17 +47,27 @@ export default class FileView extends Component {
   }
 
   changeFile(e) {
+    this.setState({isLoading: true})
+    var error
     let timelog = e.detail.timelog
     var apiRequest1 = fetch('/master.xml').then(function(response){
-      return response.text()
+      if(response.ok) {
+        return response.text()
+      } else {
+        throw new Error('Sorry, something went wrong while loading transcript.')
+      }
     });
     var apiRequest2 = fetch('/tei.xslt').then(function(response){
-      return response.text()
+      if (response.ok) {
+        return response.text()
+      } else {
+        throw new Error('Sorry, something when wrong when processing the transcript')
+      }
     });
     
     Promise.all([apiRequest1,apiRequest2]).then((values) => {
-      let outputXmlString = xslt(values[0], values[1]);
-      this.setState({ timelog: outputXmlString })
-    })
+      let outputXmlString = xslt(values[0], values[1])
+      this.setState({ timelog: outputXmlString, transcriptShow: true, isLoading: false })
+    }).catch(error => this.setState({ error, isLoading: false }))
   }
 }
