aa55780ca8731d689534d9e97e2fff98846023ed
George Wheeler <george@dewey4.com>
2018-07-17 12:23:39 -0700

    fixes and change java install method

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -2,6 +2,6 @@ Delayed::Worker.destroy_failed_jobs = false
 
 silence_warnings do
   Delayed::Job.const_set("MAX_ATTEMPTS", 10)
-  Delayed::Job.const_set("MAX_RUN_TIME", 4.hours
+  Delayed::Job.const_set("MAX_RUN_TIME", 4.hours)
   Delayed::Worker.max_run_time = 4.hours
 end
