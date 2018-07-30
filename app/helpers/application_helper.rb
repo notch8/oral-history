1d762007d12e66eecbc668f808cd2fe45f238017
George Wheeler <george@dewey4.com>
2018-07-30 16:23:42 -0700

    move transcripts to new spot in solr document & update view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,19 @@ module ApplicationHelper
     render 'shared/multiple', value: options[:value].uniq
   end
 
-  def children_from(document)
-    if document._source.present? && document._source["children_t"].present?
-      document._source["children_t"].map do |child|
+  def from_helper(attr, document)
+    if document._source.present? && document._source[attr].present?
+      document._source[attr].map do |child|
         JSON.parse(child)
       end
     end
+  end
+
+  def transcripts_from(document)
+    from_helper "transcripts_t", document
+  end
+
+  def children_from(document)
+    from_helper "children_t", document
   end
 end
