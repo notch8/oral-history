705b62cd3323b56fc417781710e194d8137779eb
Lea Ann Bradford <leaann@notch8.com>
2019-02-25 14:18:51 -0800

    updated args for controller method that runs the importer

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,15 +2,10 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
   
   def index
-
   end
 
   def run_import
-    args ||= {}
-    progress = true
-    limit = 20000000
-    limit = limit.to_i
-    OralHistoryItem.import(limit: limit)
+    OralHistoryItem.import({ progress: false })
   end
 
 end
