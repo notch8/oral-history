2d9673d9f2ced819af4faa69449d0294a553eee0
Lea Ann Bradford <leaann@notch8.com>
2019-03-04 12:16:06 -0800

    add helper text for transcript resize

--- app/javascript/components/file_view.jsx
+++ app/javascript/components/file_view.jsx
@@ -25,7 +25,9 @@ export default class FileView extends Component {
         }
 
         {transcript && isPlaying ? (
-          <div ref='container' className="container g-brd-primary g-brd-around rounded-0 transcript-container" dangerouslySetInnerHTML={{ __html: transcript}} />
+          <div><div ref='container' className="container g-brd-primary g-brd-around rounded-0 transcript-container" dangerouslySetInnerHTML={{ __html: transcript}} />
+          <div className="text-right drag-helper"><small className="form-text text-right">(pull down to resize)</small></div>
+          </div>
         ) : (
           <div></div>
         )}
