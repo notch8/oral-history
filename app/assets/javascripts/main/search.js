a86862880a6c593859cdbdd5622100982b41a280
Lea Ann Bradford <leaann@notch8.com>
2019-03-17 10:01:55 -0700

    search form defaults to selected value

--- app/assets/javascripts/main/search.js
+++ app/assets/javascripts/main/search.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+  let searchType = $('#search_field').val()
   $('body').on('change', '#search_field', function(e) {
-    let searchType = e.target.value;
+    searchType = e.target.value;
     let formAction 
     searchType && searchType === "Interview Information" ?  formAction = "/" : formAction = "/full_text";
     $('form.search-query-form').attr("action", formAction);
