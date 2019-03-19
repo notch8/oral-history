6ac2e5b47d1be703e65267d983427e77c5dec7e6
Lea Ann Bradford <leaann@notch8.com>
2019-03-19 13:56:50 -0700

    changed document load to turbolinks load for text search option

--- app/assets/javascripts/main/search.js
+++ app/assets/javascripts/main/search.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   let searchType = $('#search_field').val()
   $('body').on('change', '#search_field', function(e) {
     searchType = e.target.value;
@@ -6,4 +6,4 @@ $(document).ready(function() {
     searchType && searchType === "Interview Information" ?  formAction = "/" : formAction = "/full_text";
     $('form.search-query-form').attr("action", formAction);
   })
-})
+});
