02628cfde847c4648981794ceca1dbec5c963f06
Rob Kaufman <rob@notch8.com>
2019-04-03 18:54:34 +0000

    Update index_pdf_transcript_job.rb

--- app/jobs/index_pdf_transcript_job.rb
+++ app/jobs/index_pdf_transcript_job.rb
@@ -11,11 +11,12 @@ class IndexPdfTranscriptJob < ApplicationJob
     tmp_file.binmode
     `curl -o #{tmp_file.path} #{pdf_text}`
                   
-    result = SolrService.extract(path: tmp_file.path)
-    # put response in this field 
-    item.attributes['transcripts_t'] ||= []
-    item.attributes['transcripts_t'] << result[File.basename(tmp_file.path)].to_s.strip
-
-    item.index_record
+    if tmp_file.size > 0
+      result = SolrService.extract(path: tmp_file.path)
+      # put response in this field 
+      item.attributes['transcripts_t'] ||= []
+      item.attributes['transcripts_t'] << result[File.basename(tmp_file.path)].to_s.strip
+      item.index_record
+    end
   end
 end
