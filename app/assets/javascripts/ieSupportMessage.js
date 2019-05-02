f95882a1c5c17c35421eb7ac711c50e03b1566a3
Lea Ann Bradford <leaann@notch8.com>
2019-05-02 14:34:45 -0700

    moved ie warning to css rather than detect with js

--- app/assets/javascripts/ieSupportMessage.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1)) {
-    // Display IE warning message
-    $('.show-for-ie').css("display","block")
-  }
-}); 
