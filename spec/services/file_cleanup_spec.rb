f47aeb6e0c5b068afe9faad720b04a57dd6d9d3b
leaannbradford <ltrammer@gmail.com>
2022-07-22 11:49:03 -0700

    Refactor rspec test

--- spec/services/file_cleanup_spec.rb
+++ spec/services/file_cleanup_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe FileCleanup, clean: true do
-  it "purges files" do
-    tmp_file = Tempfile.create(['tmp_1', '.ffmpeg'], 'tmp')
-    dir = Rails.root.join('tmp')
-    files = Dir.glob(dir)
-    file_count = Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
-    new_time = Time.now - 5.hours
-    FileUtils.touch tmp_file, :mtime => new_time
+describe FileCleanup do
+  let(:new_file) { Tempfile.create(['tmp_1', '.ffmpeg'], 'tmp') }
+  let(:old_file) { Tempfile.create(['tmp_1', '.ffmpeg'], 'tmp') }
+
+  after(:each) do
+    File.delete(new_file) if File.exist?(new_file)
+  end
+
+  it "purges ffmpeg files that are over 4 hours old" do
+    FileUtils.touch old_file, :mtime => Time.now - 5.hours
+    directory = Rails.root.join('tmp')
+    files = Dir.glob(directory)
+    count_of_files_in_temp = Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count
     FileCleanup.purge_files
-    expect(Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }).to eq file_count - 1
-    expect(files).not_to include(tmp_file)
+    expect(Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count).to eq count_of_files_in_temp - 1
+    expect(files).not_to include(old_file)
+  end
+
+
+  it "doesn't delete files that are less than 4 hours old" do
+    directory = Rails.root.join('tmp')
+    files = Dir.glob(directory)
+    count_of_files_in_temp = Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count
+    FileCleanup.purge_files
+    expect(Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count).to eq count_of_files_in_temp
+    expect(File.exist?(new_file))
   end
 end
