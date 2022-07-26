1b2024f09a8b6400196eeb60f6a6e779bb60c296
Rob Kaufman <rob@notch8.com>
2022-07-26 00:38:57 +0000

    Update file_cleanup.rb

--- app/services/file_cleanup.rb
+++ app/services/file_cleanup.rb
@@ -1,9 +1,8 @@
 class FileCleanup
   def self.purge_files
-    directory = Rails.root.join('tmp')
-    Dir.glob("#{directory}/*.ffmpeg").map do |ffmpeg|
-      if File.mtime(ffmpeg) < Time.now - 4.hours
-        File.delete(ffmpeg) 
+    Dir.glob("/tmp/ffmpeg-*").map do |ffmpeg|
+      if File.mtime(ffmpeg) < (Time.now - 4.hours)
+        FileUtils.rm_rf(ffmpeg)
       end
     end
   end
