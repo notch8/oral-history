03582b8a66b6e70542993c7671de85023113da2c
leaannbradford <ltrammer@gmail.com>
2022-07-27 09:22:10 -0700

    update specs and bug fix

--- app/services/file_cleanup.rb
+++ app/services/file_cleanup.rb
@@ -1,6 +1,7 @@
 class FileCleanup
   def self.purge_files
-    Dir.glob("/tmp/ffmpeg-*").map do |ffmpeg|
+    directory = Rails.root.join('tmp')
+    Dir.glob("#{directory}/ffmpeg-*").map do |ffmpeg| 
       if File.mtime(ffmpeg) < (Time.now - 4.hours)
         FileUtils.rm_rf(ffmpeg)
       end
--- spec/services/file_cleanup_spec.rb
+++ spec/services/file_cleanup_spec.rb
@@ -1,30 +1,26 @@
 require 'rails_helper'
 
 describe FileCleanup do
-  let(:new_file) { Tempfile.create(['tmp_1', '.ffmpeg'], 'tmp') }
-  let(:old_file) { Tempfile.create(['tmp_1', '.ffmpeg'], 'tmp') }
+  let(:new_folder) { FileUtils.mkdir 'tmp/ffmpeg-1' }
+  let(:old_folder) { FileUtils.mkdir 'tmp/ffmpeg-2' }
 
   after(:each) do
-    File.delete(new_file) if File.exist?(new_file)
+    FileUtils.rm_rf(new_folder)
   end
 
   it "purges ffmpeg files that are over 4 hours old" do
-    FileUtils.touch old_file, :mtime => Time.now - 5.hours
+    FileUtils.touch old_folder, :mtime => Time.now - 5.hours
     directory = Rails.root.join('tmp')
-    files = Dir.glob(directory)
-    count_of_files_in_temp = Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count
+    count_of_folders_in_temp =  Dir.glob("#{directory}/*").count
     FileCleanup.purge_files
-    expect(Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count).to eq count_of_files_in_temp - 1
-    expect(files).not_to include(old_file)
+    expect(Dir.glob("#{directory}/*").count).to eq count_of_folders_in_temp - 1
   end
 
 
   it "doesn't delete files that are less than 4 hours old" do
     directory = Rails.root.join('tmp')
-    files = Dir.glob(directory)
-    count_of_files_in_temp = Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count
+    count_of_folders_in_temp = Dir.glob("#{directory}/*").count
     FileCleanup.purge_files
-    expect(Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count).to eq count_of_files_in_temp
-    expect(File.exist?(new_file))
+    expect(Dir.glob("#{directory}/*").count).to eq count_of_folders_in_temp
   end
 end
