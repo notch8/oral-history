fd543595260794a08a17d5dd3c9ed8b711330337
April Rieger <april.rieger@yahoo.com>
2023-03-15 10:03:15 -0700

    ran rake app:update in the running applications web container

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,19 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,18 +24,12 @@ module OralHistory
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-
-    config.active_job.queue_adapter = :delayed_job
-    BlacklightDynamicSitemap::Engine.config.max_documents_expiration = 1.day
-    BlacklightDynamicSitemap::Engine.config.minimum_average_chunk = 10_000
-    BlacklightDynamicSitemap::Engine.config.hashed_id_field = 'hashed_id_ssi'
-    BlacklightDynamicSitemap::Engine.config.unique_id_field = 'id'
-    BlacklightDynamicSitemap::Engine.config.last_modified_field = 'timestamp'
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
