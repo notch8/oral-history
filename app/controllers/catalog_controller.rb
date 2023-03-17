a675fb88a5f231627b308c7217e2429428acb96f
Summer Cook <summer@scientist.com>
2023-03-17 14:31:33 -0500

    first round of updated views

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 class CatalogController < ApplicationController
+
   include Blacklight::Catalog
   include Blacklight::Marc::Catalog
   include Blacklight::DefaultComponentConfiguration
   before_action :setup_negative_captcha, only: [:email]
+
   configure_blacklight do |config|
     ## Class for sending and receiving requests from a search index
     # config.repository_class = Blacklight::Solr::Repository
@@ -13,6 +15,7 @@ class CatalogController < ApplicationController
     #
     ## Model that maps search index responses to the blacklight response model
     # config.response_model = Blacklight::Solr::Response
+
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
       rows: 10,
@@ -29,24 +32,11 @@ class CatalogController < ApplicationController
     # hl.simple.pre and hl.simple.post will not get truncated.
 
     # solr path which will be added to solr base url before the other solr params.
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @@ -119,7 +119,7 @@ class CatalogController < ApplicationController
-
     #config.solr_path = 'select'
+
     # items to show per page, each number in the array represent another option to choose from.
     #config.per_page = [10,20,50,100]
+
     ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SearchHelper#solr_doc_params) or
     ## parameters included in the Blacklight-jetty document requestHandler.
     #
@@ -64,14 +54,17 @@ class CatalogController < ApplicationController
       :"hl.simple.post" => "</span>",
       :"hl.alternateField" => "dd"
     }
+
     # solr field configuration for search results/index views
     config.index.title_field = 'title_display'
     config.index.display_type_field = 'format'
     #config.index.thumbnail_field = 'thumbnail_path_ss'
+
     # solr field configuration for document/show views
     #config.show.title_field = 'title_display'
     #config.show.display_type_field = 'format'
     #config.show.thumbnail_field = 'thumbnail_path_ss'
+
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
     #
@@ -95,6 +88,7 @@ class CatalogController < ApplicationController
     # set :index_range to true if you want the facet pagination view to have facet prefix-based navigation
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
+
     #config.add_facet_field 'format', label: 'Format'
 #    config.add_facet_field 'pub_date', label: 'Publication Year', single: true
     config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
@@ -106,15 +100,19 @@ class CatalogController < ApplicationController
     #config.add_facet_field 'type_of_resource_facet', label: 'Type of Resource'
     config.add_facet_field 'audio_b', label: 'Has Media', helper_method: 'audio_icon_with_text'
     #config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
+
 #    config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
 #       :years_5 => { label: 'within 5 Years', fq: "pub_date:[#{Time.zone.now.year - 5 } TO *]" },
 #       :years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
 #       :years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
 #    }
+
+
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
     # handler defaults, or have no facets.
     config.add_facet_fields_to_solr_request!
+
     # solr fields to be displayed in the index (search results and bookmarks) view
     #   The ordering of the field names is the order of the display
     # config.add_index_field 'subtitle_display', label: 'Subtitle'
@@ -124,21 +122,6 @@ class CatalogController < ApplicationController
     config.add_index_field 'audio_b', label: 'Audio', highlight: true, solr_params: { :"hl.alternateField" => "dd" }, helper_method: 'audio_icon'
     config.add_index_field 'abstract_t', label: 'Series Statement', highlight: true, solr_params: { :"hl.alternateField" => "dd", :"hl.maxAlternateFieldLength" => 0, :"hl.highlightAlternate" => true  }, helper_method: 'index_filter'
     config.add_index_field 'biographical_t', label: 'Biographical Note', highlight: true, solr_params: { :"hl.alternateField" => "dd", :"hl.maxAlternateFieldLength" => 0, :"hl.highlightAlternate" => true  }, helper_method: 'index_filter'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @@ -249,6 +249,7 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
@@ -174,6 +157,7 @@ class CatalogController < ApplicationController
  #   config.add_show_field 'published_vern_display', label: 'Published'
  #   config.add_show_field 'lc_callnum_display', label: 'Call number'
  #   config.add_show_field 'isbn_t', label: 'ISBN'
+
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
     #
@@ -187,16 +171,22 @@ class CatalogController < ApplicationController
     # as well as in URLs -- so changing it after deployment may break bookmarked
     # urls.  A display label will be automatically calculated from the :key,
     # or can be specified manually to be different.
+
     # This one uses all the defaults set by the solr request handler. Which
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
+
     config.add_search_field 'all_fields', label: 'All Fields'
+
+
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
+
     config.add_search_field('title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params.
       field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
@@ -206,6 +196,7 @@ class CatalogController < ApplicationController
         pf: '$title_pf'
       }
     end
+
     config.add_search_field('author') do |field|
       field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
       field.solr_local_parameters = {
@@ -213,6 +204,7 @@ class CatalogController < ApplicationController
         pf: '$author_pf'
       }
     end
+
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as
     # config[:default_solr_parameters][:qt], so isn't actually neccesary.
@@ -224,6 +216,8 @@ class CatalogController < ApplicationController
         pf: '$subject_pf'
       }
     end
+
+
     config.add_search_field('biographical') do |field|
       field.solr_parameters = { :'spellcheck.dictionary' => 'biographical' }
       field.solr_local_parameters = {
@@ -231,6 +225,7 @@ class CatalogController < ApplicationController
         pf: '$biographical_pf'
       }
     end
+
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
@@ -239,52 +234,54 @@ class CatalogController < ApplicationController
     config.add_sort_field 'series_sort asc, title_sort asc', label: 'Series'
     config.add_sort_field 'interviewee_sort asc, title_sort asc', label: 'Interviewee'
     config.add_sort_field 'language_sort asc, title_sort asc', label: 'Language'
+
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
     config.spell_max = 5
+
     # Configuration for autocomplete suggestor
     config.autocomplete_enabled = false
     # config.autocomplete_path = 'suggest'
+
     config.add_field_configuration_to_solr_request!
+
   end
 
   # Override to add highlighing to show - from Blacklight 6.23
   # Currently using Blacklight 7.10 default show method
+
   # TODO(April): update highlighting on show page
 
   def show
-
-
-
-
-
-
-
-
-
-
     deprecated_response, @document = search_service.fetch(params[:id])
+
     @response = ActiveSupport::Deprecation::DeprecatedObjectProxy.new(deprecated_response, 'The @response instance variable is deprecated; use @document.response instead.')
+
     # We can not get highlight on get query, and are not getting child documents on select query
     # so we make a second call to solr in order to get the highlight. not super efficient, but effective
     highlight_response, highlight_document = search_service.fetch([params[:id]], {
       :"hl.q" => current_search_session.try(:query_params).try(:[], "q"),
       :df => blacklight_config.try(:default_document_solr_params).try(:[], :"hl.fl")
     })
+
     @response['highlighting'] = highlight_response['highlighting']
     @document.response['highlighting'] = highlight_response['highlighting']
+
     respond_to do |format|
       format.html { @search_context = setup_next_and_previous_documents }
       format.json
       additional_export_formats(@document, format)
     end
   end
+
   # override from blacklight 6.12 to handle captcha
   def email
     @response, @documents = action_documents
+
     if request.post? && validate_email_params
       email_action(@documents)
       flash[:success] ||= I18n.t("blacklight.email.success", default: nil)
+
       respond_to do |format|
         format.html do
           return render "email_success", layout: false if request.xhr?
@@ -300,6 +297,7 @@ class CatalogController < ApplicationController
       end
     end
   end
+
   private
   def setup_negative_captcha
     @captcha = NegativeCaptcha.new(
@@ -313,6 +311,7 @@ class CatalogController < ApplicationController
       params: params
     )
   end
+
   # Email Action (this will render the appropriate view on GET requests and process the form and send the email on POST requests)
   def email_action documents
     mail = RecordMailer.email_record(documents, {:to => @captcha.values[:to], :message => @captcha.values[:message]}, url_options)
@@ -322,6 +321,7 @@ class CatalogController < ApplicationController
       mail.deliver
     end
   end
+
   def validate_email_params
     if !@captcha.valid?
       flash[:error] = @captcha.message
@@ -330,6 +330,7 @@ class CatalogController < ApplicationController
     elsif !@captcha.values[:to].match(Blacklight::Engine.config.email_regexp)
       flash[:error] = I18n.t('blacklight.email.errors.to.invalid', :to => @captcha.values[:to])
     end
+
     flash[:error].blank?
   end
 end
