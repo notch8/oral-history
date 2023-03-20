ad10e66fc3b920e77e82868abb605c8a8d9fab34
April Rieger <april.rieger@yahoo.com>
2023-03-20 09:50:17 -0700

    Adds mil check if total_records is nil

--- app/jobs/import_records_job.rb
+++ app/jobs/import_records_job.rb
@@ -5,7 +5,7 @@ class ImportRecordsJob < ProgressJob::Base
 
   def perform
     update_stage('Importing Records')
-    update_progress_max(OralHistoryItem.total_records)
+    update_progress_max(OralHistoryItem.total_records) unless OralHistoryItem.total_records.nil?
     OralHistoryItem.import({ limit: 20000000, progress: false }.merge(@args)) do |total|
       update_progress
     end
