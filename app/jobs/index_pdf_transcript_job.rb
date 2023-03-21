8c0affc96a12b29ff87f32eeb887b1ab4ccd3220
April Rieger <april.rieger@yahoo.com>
2023-03-21 11:43:05 -0700

    Updates for the brakeman audit

--- app/jobs/index_pdf_transcript_job.rb
+++ app/jobs/index_pdf_transcript_job.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 class IndexPdfTranscriptJob < ApplicationJob
   queue_as :default
 
@@ -7,13 +9,15 @@ class IndexPdfTranscriptJob < ApplicationJob
     item = OralHistoryItem.find_or_new(id)
     # make call to solr for extraction
     tmp_file = Tempfile.new
-    
+
     tmp_file.binmode
-    `curl -o #{tmp_file.path} #{pdf_text}`
-                  
+    escaped_pdf_text = Shellwords.escape(pdf_text)
+    cmd = "curl -o #{tmp_file.path} #{escaped_pdf_text}"
+    system(cmd)
+
     if tmp_file.size > 0
       result = SolrService.extract(path: tmp_file.path)
-      # put response in this field 
+      # put response in this field
       item.attributes['transcripts_t'] ||= []
       item.attributes['transcripts_t'] << result[File.basename(tmp_file.path)].to_s.strip
       item.index_record
